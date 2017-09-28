62346a00650758d121da6f91448e4a1298b92310
smalls <stsmall@gmail.com>
2017-09-28 16:37:15 -0400

    d

--- fish_circos.py
+++ fish_circos.py
@@ -56,7 +56,7 @@ def addmaptoaln(infile, ddfish):
         with open(infile, 'r') as nuc:
             for line in nonblank_lines(nuc):
                 if line.startswith('['):
-                    f.write(line)
+                    f.write("{}\n".format(line))
                 elif line.strip().split()[0].isdigit():
                     x = line.strip().split()
                     for key in ddfish.keys():
@@ -65,49 +65,52 @@ def addmaptoaln(infile, ddfish):
                             x[-1] = chrom
                             f.write("{}\n".format("\t".join(x)))
                         else:
-                            f.write(line)
+                            f.write("{}\n".format(line))
                 else:
-                    f.write(line)
+                    f.write("{}\n".format(line))
     return(None)
 
 
 def makelinks(ddfish, outfile, infile, size=5000):
     """links are from - to, but files are named to - from
     """
-    qs = []
-    qn = []
-    ss = []
-    sn = []
-    with open("circos.{}.links.txt".format(outfile), 'w') as f:
-        with open(infile, 'r') as nuc:
-            for line in nonblank_lines(nuc):
-                if line.startswith('['):
-                    h = line.strip().split()
-                    header = makeheader(h)
-                elif line.strip().split()[0].isdigit():
-                    x = line.strip().split()
-                    alnlen = "LEN 1".index(header)
-                    if int(x[alnlen]) >= size:
-                        for key in ddfish.keys():
-                            if x[-1] in ddfish[key]:
-                                chrom = "{}_{}".format(key, x[-1])
-                                x[-1] = chrom
-                                qn.append(chrom)
-                                lenq = "LEN Q".index(header)
-                                qs.append(x[lenq])
-                                lens = "LEN R".index(header)
-                                ss.append(x[lens])
-                                sn.append(x[-2])
-                                rstart = "S1".index(header)
-                                rend = "E1".index(header)
-                                qstart = "S2".index(header)
-                                qend = "E2".index(header)
-                                f.write("{}\n".format(" ".join([x[-1],
-                                                               x[qstart],
-                                                               x[qend],
-                                                               x[-2],
-                                                               x[rstart],
-                                                               x[rend]])))
+    try:
+        qs = []
+        qn = []
+        ss = []
+        sn = []
+        with open("circos.{}.links.txt".format(outfile), 'w') as f:
+            with open(infile, 'r') as nuc:
+                for line in nonblank_lines(nuc):
+                    if line.startswith('['):
+                        h = line.strip().split()
+                        header = makeheader(h)
+                    elif line.strip().split()[0].isdigit():
+                        x = line.strip().split()
+                        alnlen = "LEN 1".index(header)
+                        if int(x[alnlen]) >= size:
+                            for key in ddfish.keys():
+                                if x[-1] in ddfish[key]:
+                                    chrom = "{}_{}".format(key, x[-1])
+                                    x[-1] = chrom
+                                    qn.append(chrom)
+                                    lenq = "LEN Q".index(header)
+                                    qs.append(x[lenq])
+                                    lens = "LEN R".index(header)
+                                    ss.append(x[lens])
+                                    sn.append(x[-2])
+                                    rstart = "S1".index(header)
+                                    rend = "E1".index(header)
+                                    qstart = "S2".index(header)
+                                    qend = "E2".index(header)
+                                    f.write("{}\n".format(" ".join([x[-1],
+                                                                   x[qstart],
+                                                                   x[qend],
+                                                                   x[-2],
+                                                                   x[rstart],
+                                                                   x[rend]])))
+    except:
+        import ipdb;ipdb.set_trace()
     return(zip(sorted(set(sn), key=sn.index), sorted(set(ss), key=ss.index)),
            zip(sorted(set(qn), key=qn.index), sorted(set(qs), key=qs.index)))
 
