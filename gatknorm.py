3f766cf363d3a3c4e52afd13b38bcbf8cbb4694b
smalls <stsmall@gmail.com>
2017-12-02 15:28:46 -0500

    d

--- gatknorm.py
+++ gatknorm.py
@@ -28,48 +28,43 @@ def fixPGTPID(vcf):
                 formats = x[8].split(":")
                 if x[5] == 'inf':
                     x[5] = '500'
-                else:
-                    pass
+                if "<NON_REF>" in x[4]:
+                    x[4] = "."
                 if "*" not in x[4] and len(formats) > 1:
                     if "PGT" in formats or "PID" in formats:
-                        try:
-                            for sample in range(9, len(x)):
-                                gt = x[sample].split(":")
-                                ad = gt[formats.index('AD')]
-                                dp = gt[formats.index('DP')]
-                                gq = gt[formats.index('GQ')]
-                                pl = gt[formats.index('PL')]
-                                newgt = [gt[0], ad, dp, gq, pl]
-                                x[sample] = ":".join(newgt)
-                            x[8] = "GT:AD:DP:GQ:PL"
-                            f.write("{}\n".format("\t".join(x)))
-                        except:
-                            import ipdb;ipdb.set_trace()
+                        for sample in range(9, len(x)):
+                            gt = x[sample].split(":")
+                            ad = gt[formats.index('AD')]
+                            dp = gt[formats.index('DP')]
+                            gq = gt[formats.index('GQ')]
+                            pl = gt[formats.index('PL')]
+                            newgt = [gt[0], ad, dp, gq, pl]
+                            x[sample] = ":".join(newgt)
+                        x[8] = "GT:AD:DP:GQ:PL"
+                        f.write("{}\n".format("\t".join(x)))
                     elif "." in x[4]:
                         if len(x[3]) > 1:
+                            # skip ref allele that are insertions
                             continue
                         else:
                             # fix invariant
-                            try:
-                                for sample in range(9, len(x)):
-                                    gt = x[sample].split(":")
-                                    try:
-                                        gq = gt[formats.index('RGQ')]
-                                    except ValueError:
-                                        gq = '99'
-                                    dp = gt[formats.index('DP')]
-                                    try:
-                                        ad = gt[formats.index('AD')]
-                                    except ValueError:
-                                        ad = dp
-                                    pl = '0'
-                                    adv = ad.split(",")[0]
-                                    newgt = [gt[0], adv, dp, gq, pl]
-                                    x[sample] = ":".join(newgt)
-                                x[8] = "GT:AD:DP:GQ:PL"
-                                f.write("{}\n".format("\t".join(x)))
-                            except ValueError:
-                                import ipdb;ipdb.set_trace()
+                            for sample in range(9, len(x)):
+                                gt = x[sample].split(":")
+                                try:
+                                    gq = gt[formats.index('RGQ')]
+                                except ValueError:
+                                    gq = '99'
+                                dp = gt[formats.index('DP')]
+                                try:
+                                    ad = gt[formats.index('AD')]
+                                except ValueError:
+                                    ad = dp
+                                pl = '0'
+                                adv = ad.split(",")[0]
+                                newgt = [gt[0], adv, dp, gq, pl]
+                                x[sample] = ":".join(newgt)
+                            x[8] = "GT:AD:DP:GQ:PL"
+                            f.write("{}\n".format("\t".join(x)))
                     else:
                         f.write(line)
     f.close()
