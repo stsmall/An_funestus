d884dc129ba5a60cc29f9cb8642fe1a582e7c0df
smalls <stsmall@gmail.com>
2018-01-20 15:59:02 -0500

    calculate node age and support for a monophyletic group

--- calcNodeHeights.py
+++ calcNodeHeights.py
@@ -11,7 +11,7 @@ from __future__ import division
 import numpy as np
 from ete3 import PhyloTree
 import argparse
-from itertools import combinations
+
 parser = argparse.ArgumentParser()
 parser.add_argument('-t', "--treefile", type=str, required=True,
                     help="treefile in newick, 1 per line")
@@ -22,46 +22,10 @@ parser.add_argument('-w', "--windows", type=str,
                     help="coordinates for each tree")
 parser.add_argument("--dlm", type=str, default=".",
                     help="delimeter denoting species")
-parser.add_argument("--nodes", action="store_true",
-                    help="calculate node heights for a given quartet")
 args = parser.parse_args()
 
 
-def cMono(tree, taxon):
-    """Checks if samples are monophyletic in tree
-    """
-    return(tree.check_monophyly(values=[taxon], target_attr="species")[0])
-
-
-def getMonophyletic(treelist, quart, winarray):
-    """
-    """
-    p1, p2, p3, p4 = quart
-    mtreelist = []
-    for i, t in enumerate(treelist):
-        if cMono(t, p1) and cMono(t, p2) and cMono(t, p3) and cMono(t, p4):
-            Out = t.get_common_ancestor(t.search_nodes(species=quart[-1]))
-            t.set_outgroup(Out)
-            mtreelist.append(t)
-        else:
-            winarray[i] = False
-#            t2 = t.collapse_lineage_specific_expansions()
-#            print(t2)
-#            for node in t.split_by_dupes():
-#                print(node)
-#            ntrees, ndups, sptrees = t.get_speciation_trees()
-#            for spt in sptrees:
-#                print(spt)
-    return(mtreelist, winarray)
-
-
-def supportFilt(mtreelist, quart, winarray):
-    """
-    """
-    return(mtreelist, winarray)
-
-
-def loadtrees(treefile, quart, winlist, winarray, nodes, dlm):
+def LoadTrees(treefile, quart, outgroup, dlm):
     """Reads and stores phylogenetic trees from a file
 
     Parameters
@@ -81,52 +45,78 @@ def loadtrees(treefile, quart, winlist, winarray, nodes, dlm):
             if not line.startswith("NA"):
                 t = PhyloTree(line)
                 t.set_species_naming_function(lambda node: node.name.split(dlm)[0])
+                t.set_outgroup( t&outgroup )
                 treelist.append(t)
-    if not winlist:
-        winarray = np.ones(len(treelist), dtype=bool)
-    mtreelist, winarray = getMonophyletic(treelist, quart, winarray)
-    btreelist, winarray = supportFilt(mtreelist, quart, winarray)
-    if nodes:
-        nh1, nh2 = nodeHeights(btreelist, quart)
-    else:
-        nh1 = []
-        nh2 = []
-    return(treelist, winarray, nh1, nh2)
+    return(treelist)
 
 
-def nodeHeights(mtreelist, quart):
-    """Calculates the node heights in a set of trees.
+def WriteTrees(treelist):
+    """Rewrite reformatted or rerooted trees
 
     Parameters
     ------
-    trees: ete3 object, returned from function loadtrees
-    quart: list, list of topologies to calculate node heights
-    windows: list, genome coordinates for which the trees were made
+    treelist: list, list of tree objects
 
     Returns
     ------
-    nh1: float, average value of T1 for topology
-    nh2: float, average value of T2 for topology
+    file: file, writes out tree objects in nexus format
+
     """
-    print("calculating node heights for quartets: {}".format(quart))
-    nh1 = []
-    nh2 = []
-    for t in mtreelist:
-        P1 = t.search_nodes(species=quart[0])
-        P2 = t.search_nodes(species=quart[1])
-        P3 = t.search_nodes(species=quart[2])
-        for p1, p2 in combinations(P1, P2, 2):
-            nh2.append(t.get_distance(p1, p2))
-        for p1, p3 in combinations(P1, P3, 2):
-            nh1.append(t.get_distance(p1, p3))
-        for p2, p3 in combinations(P2, P3, 2):
-            nh1.append(t.get_distance(p2, p3))
-    print("T1: {}, T2: {}".format(np.mean(np.array(nh1)),
-                                  np.mean(np.array(nh2))))
-    return(nh1, nh2)
+    f = open("rooted_trees.nex", 'a')
+    for t in treelist:
+        t.write(outfile=f)
+    f.close()
+    return(None)
 
 
-def parseWin(windows):
+def cMono(tree, taxon):
+    """Checks if samples are monophyletic in tree
+    """
+    return(tree.check_monophyly(values=[taxon], target_attr="species")[0])
+
+
+def AgeAndSupport(treelist, taxon):
+    """Calculates the support and node age if groups in taxon are monophyletic
+    """
+    taxdict = {}
+    for i, tax in enumerate(taxon):
+        nodesupport = []
+        nodeage = []
+        for t in treelist:
+            if cMono(t, tax):
+                samples = []
+                for sp in tax:
+                    samples.extend(t.search_nodes(species=sp))
+                ancnode = t.get_common_ancestor(samples)
+                nodeage.append(ancnode.dist)
+                nodesupport.append(ancnode.support)
+                # alternate way
+#                t2 = t.collapse_lineage_specific_expansions()
+#                samples = t2.get_leaf_names()
+#                sampletax = [i.split("_")[0] for i in samples]
+#                ix = []
+#                for species in tax:
+#                    ix.append(samples[species.index(sampletax)])
+#                nd = t2.get_common_ancestor(ix)
+#                nd.dist
+#                nd.support
+        taxdict[i] = (nodeage, nodesupport)
+    return(taxdict)
+
+
+def PlotStats(taxdict):
+    """Plot histogram and stats for branch lengths and support
+    """
+    return(None)
+
+
+def SupportFilt(treelist, quart):
+    """Collapses nodes or make polytomy with support below threshold
+    """
+    return(None)
+
+
+def ParseWin(windows):
     """
     """
     winlist = []
@@ -139,20 +129,17 @@ def parseWin(windows):
     return(winlist, winarray)
 
 
-def outputTrees(treelist, winlist, winarray, nh1, nh2):
+def TreesTable(treelist, winlist, winarray, nh1, nh2):
+    """Print windowed statistics for trees, also print corresponding trees
     """
-    """
+    # remove trees with low support
+    # write rerooted trees
     # mask winlist and treelist using winarray T/F
     return(None)
 
 
 if __name__ == "__main__":
     quart = args.groups
-    if args.windows:
-        winlist, winarray = parseWin(args.windows)
-    else:
-        winlist = []
-        winarray = []
-    treelist, winarray, nh1, nh2 = loadtrees(args.treefile, quart, winlist,
-                                             winarray, args.nodes, args.dlm)
-    outputTrees(treelist, winlist, winarray, nh1, nh2)
+    treelist = LoadTrees(args.treefile, quart, args.outgroup, args.dlm)
+    WriteTrees(treelist)
+    taxdict = AgeAndSupport(treelist, quart)
