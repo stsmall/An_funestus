4f0596d074982e80244d007c471e5b14343ca19f
smalls <stsmall@gmail.com>
2018-01-21 14:18:03 -0500

    d

--- calcT1T2-permutation.py
+++ calcT1T2-permutation.py
@@ -72,8 +72,9 @@ def loadvcf(vcFile, quart, dlm):
                                 count_list.append([ref, alt])
                         if "0/1" not in polarize or "0|1" not in polarize or "1|0" not in polarize:
                             # if ancestral is not polymorphic
+                            if count_list is None:
+                                import ipdb;ipdb.set_trace()
                             qdict[chrom][pos] = (count_list)
-    import ipdb;ipdb.set_trace()
     return(qdict, q_ix, samplelist, callabledict)
 
 
