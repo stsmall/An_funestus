e261758ed9a22ca019147d22b2ab44a891f08ad2
smalls <stsmall@gmail.com>
2018-03-05 13:54:55 -0500

    d

--- fillMAF.py
+++ fillMAF.py
@@ -65,9 +65,12 @@ def getMAFambig(mafFile):
                 while line.startswith("s"):
                     x = line.split()
                     ind, chrom = x[1].split(".")
-                    ######
-                    ipdb.set_trace()
-                    block_key = "{}_{}_{}_{}_{}".format(x[1:6])
+
+                    try:
+                        block_key = "{}_{}_{}_{}_{}".format(x[1:6])
+                    except IndexError:
+                        ipdb.set_trace()
+
                     coord, poslist = transCoord(x[2:])  # coord is list of N in block
                     mafdict[ind][chrom].append([block_key, coord, poslist, "N"])
                     line = next(maf)
