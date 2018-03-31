4502229c57e0a88232912bad3b197a7974472c51
smalls <stsmall@gmail.com>
2018-03-31 15:51:19 -0400

    d

--- divTime.py
+++ divTime.py
@@ -143,8 +143,8 @@ def countPatternsFast(callset, pops, outgroup):
     if len(pops[1]) == 1:
         hap2 = pops[1]
         for hap1 in list(range(len(pops[0]))):
-            ma = htA[:, [hap1]].count_alleles(max_allel=1)
-            mb = htB[:, hap2].count_alleles(max_allel=1)
+            ma = htA[:, [hap1]].count_alleles(max_allele=1)
+            mb = htB[:, hap2].count_alleles(max_allele=1)
             jsfs = allel.joint_sfs(ma[:, 1], mb[:, 1])
             n1 = jsfs[0, 2] + jsfs[1, 0]
             n2 = jsfs[0, 1] + jsfs[1, 1]
@@ -157,8 +157,8 @@ def countPatternsFast(callset, pops, outgroup):
     else:
         for hap1 in list(range(len(pops[0]))):
             for hap2 in list(combinations(range(len(pops[1])), 2)):
-                ma = htA[:, [hap1]].count_alleles(max_allel=1)
-                mb = htB[:, hap2].count_alleles(max_allel=1)
+                ma = htA[:, [hap1]].count_alleles(max_allele=1)
+                mb = htB[:, hap2].count_alleles(max_allele=1)
                 jsfs = allel.joint_sfs(ma[:, 1], mb[:, 1])
                 n1 = jsfs[0, 2] + jsfs[1, 0]
                 n2 = jsfs[0, 1] + jsfs[1, 1]
