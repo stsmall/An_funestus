8e81f20dbfac0b6f26691adfd376c0b017b17b35
smalls <stsmall@gmail.com>
2018-04-02 16:25:40 -0400

    d

--- divTime.py
+++ divTime.py
@@ -144,7 +144,9 @@ def countN1N2N3Fast(gt, pops):
                 n2 = jsfs[0, 1] + jsfs[1, 1]
                 n3 = jsfs[0, 0] + jsfs[1, 2]
             except IndexError:
-                import ipdb;ipdb.set_trace()
+                n1 = 0 + jsfs[1, 0]
+                n2 = jsfs[0, 1] + jsfs[1, 1]
+                n3 = jsfs[0, 0] + 0
             # fast approx
             k_hat = .75 * ((2*n3 + n2) / (n1 + n2 + n3))
             c_hat = (2*n3 - n2) / (2*n3 + n2)
@@ -161,7 +163,9 @@ def countN1N2N3Fast(gt, pops):
                     n2 = jsfs[0, 1] + jsfs[1, 1]
                     n3 = jsfs[0, 0] + jsfs[1, 2]
                 except IndexError:
-                    import ipdb;ipdb.set_trace()
+                    n1 = 0 + jsfs[1, 0]
+                    n2 = jsfs[0, 1] + jsfs[1, 1]
+                    n3 = jsfs[0, 0] + 0
                 # fast approx
                 k_hat = .75 * ((2*n3 + n2) / (n1 + n2 + n3))
                 c_hat = (2*n3 - n2) / (2*n3 + n2)
