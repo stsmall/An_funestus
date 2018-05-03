521c3a423f5be70809d8da47b467d642e56dc228
smalls <stsmall@gmail.com>
2018-05-03 17:59:37 -0400

    d

--- format4phylonet.py
+++ format4phylonet.py
@@ -79,7 +79,9 @@ def phylonetSeq(ntax, tchar, seqdict, phylonetcmd):
         for seq in seqdict[locus]:
             try:
                 name, dna = seq.split()
-                f.write("{}\t{}\n".format(name.split(":")[0], dna))
+                f.write("{}\n".format(name.split(":")[0]))
+                f.write("{}\n".format(dna))
+                #f.write("{}\t{}\n".format(name.split(":")[0], dna))
             except ValueError:
                 # import ipdb;ipdb.set_trace()
                 pass
