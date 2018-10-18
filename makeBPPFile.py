23544a1e2babb5d08bda35f4f11c7c952f22d331
smalls <stsmall@gmail.com>
2018-10-18 14:26:07 -0400

    use fasta and bed to make bpp file

---
+++ makeBPPFile.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 17 16:23:36 2018
+python makeBPPfile.py --gff gff.bed --distance 2000 --length 1000 --fasta FOO.fa
+@author: scott
+"""
+
+import argparse
+from Bio import SeqIO
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--gff", type=str, required=True, help="bed file from gff")
+parser.add_argument("--distance", type=int, default=2000, help="distance"
+                    " between non-coding loci")
+parser.add_argument("--length", type=int, default=1000, help="length for"
+                    " non-coding loci")
+parser.add_argument("--fasta", type=str, required=True, help="fasta file")
+parser.add_argument("--clust", type=int, default=1000000, help="how many loci"
+                    " to cluster into 1 file")
+parser.add_argument('-s', "--samples", type=int, required=True)
+args = parser.parse_args()
+
+
+def getCDS(gffFile):
+    """
+    """
+    cdsdict = {}
+    i = 0
+    with open(gffFile, 'r') as gff:
+        for line in gff:
+            x = line.split()
+            chrom = x[0]
+            feature = x[2]
+            start = x[3]
+            end = x[4]
+            direction = x[6]  # reverse complement
+            if "CDS" in feature:
+                cdsdict["cds_" + str(i)] = [int(start)-1, int(end), direction]
+                i += 1
+    return(cdsdict)
+
+
+def getNonCDS(cdsdict, lengths, distance):
+    """
+    """
+    noncdsdict = {}
+    init = 0
+    for i, k in enumerate(cdsdict.keys()):
+        end = cdsdict[k][1]
+        next_start = cdsdict["cds_" + str(i+1)][0]
+        if init == 0:
+            end = 0
+            next_start = cdsdict[k][0]
+        Sstart = end + distance
+        Send = Sstart + lengths
+        if next_start - Send > distance:
+            noncdsdict["ncds" + str(i)] = [int(Sstart)-1, int(Send)]
+    return(noncdsdict)
+
+
+def bppFormat(CDSdict, nonCDSdict, fastaFile, clust, samples):
+    """
+    """
+    fasta_sequences = SeqIO.parse(fastaFile, 'fasta')
+    # CDS
+    loci = 1
+    out_file = open("CDS.bpp.{}.txt".format(loci), 'w')
+    for cds in CDSdict:
+        if loci > clust:
+            out_file.close()
+            out_file = open("CDS.bpp.{}.txt".format(loci), 'w')
+        x = CDSdict[cds]
+        length = x[1] - x[0]
+        out_file.write("\n{} {}\n\n".format(samples, length))
+        for fasta in fasta_sequences:
+            # read in header and sequence
+            header, sequence = fasta.id, str(fasta.seq)
+            locus = sequence[x[0]:x[1]]
+            out_file.write("^{} {}\n".format(header, locus))
+        loci += 1
+    out_file.close()
+    # nonCDS
+    loci = 1
+    out_file = open("nCDS.bpp.{}.txt".format(loci), 'w')
+    for cds in nonCDSdict:
+        if loci > clust:
+            out_file.close()
+            out_file = open("nCDS.bpp.{}.txt".format(loci), 'w')
+        x = CDSdict[cds]
+        length = x[1] - x[0]
+        out_file.write("\n{} {}\n\n".format(samples, length))
+        for fasta in fasta_sequences:
+            # read in header and sequence
+            header, sequence = fasta.id, str(fasta.seq)
+            locus = sequence[x[0]:x[1]]
+            out_file.write("^{} {}\n".format(header, locus))
+        loci += 1
+    out_file.close()
+    return(None)
+
+
+if __name__ == "__main__":
+    gffFile = args.gff
+    CDSdict = getCDS(gffFile)
+    nonCDSdict = getNonCDS(CDSdict, args.length, args.distance)
+    bppFormat(CDSdict, nonCDSdict, args.fasta, args.clust, args.samples)
