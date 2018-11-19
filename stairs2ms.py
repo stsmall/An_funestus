4bcc48ffb3d9fbb6afd52466bd4f3e51248b645b
smalls <stsmall@gmail.com>
2018-11-19 17:08:27 -0500

    recode stairplot output to use in discoal

---
+++ stairs2ms.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 19 16:42:04 2018
+stairway2ms.py -i stairway.final.summary -L 1000
+@author: scott
+"""
+
+from __future__ import print_function
+from __future__ import division
+import argparse
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--inFile", type=str, required=True, help="stairway file")
+parser.add_argument("--LocusLen", type=int, default=1000, help="locus length")
+parser.add_argument("--rhorat", type=float, help="ratio of mu/rho")
+args = parser.parse_args()
+
+
+def stairs2ms(inFile, locLen, rhorat):
+    """
+    """
+    msmc2ms = []
+    with open(inFile,  'r') as stair:
+        next(stair)
+        line = next(stair)
+        x = line.split()
+        theta0M = float(x[2])
+        theta0L = float(x[3])
+        theta0U = float(x[4])
+        Ne0 = float(x[6])
+        for line in stair:
+            x = line.split()
+            theta = float(x[2])
+            thetaL = float(x[3])
+            thetaU = float(x[4])
+            gens = float(x[5])
+            Ne = float(x[6])
+            msmc2ms.append([gens/(4*Ne0), theta/theta0M])
+    tM = theta0M * locLen
+    tL = theta0L * locLen
+    tU = theta0U * locLen
+    size = ' -en '.join(' '.join(str(x) for x in timestep) for timestep in msmc2ms)
+    print("-Pt {} {}".format(tL, tU))
+    print("-Pr {} {}".format(tL*rhorat, tU*rhorat))
+    print("-t {} -r {} -en {}".format(tM, tM * rhorat, size))
+    return(None)
+
+
+if __name__ == "__main__":
+    stairs2ms(args.inFile, args.LocusLen, args.rhorat)
