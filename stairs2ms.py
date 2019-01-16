c1bff91150ef5b297e6ef1c4040043db37a02608
smalls <stsmall@gmail.com>
2019-01-16 14:12:23 -0500

    works iwth misorient and discoal

--- stairs2ms.py
+++ stairs2ms.py
@@ -16,29 +16,31 @@ parser.add_argument('-i', "--inFile", type=str, required=True, help="stairway fi
 parser.add_argument('-L', "--LocusLen", type=int, default=1000, help="locus length")
 parser.add_argument('-r', "--rhorat", type=float, help="ratio of mu/rho")
 parser.add_argument('-e', "--epochs", type=int, help="epochs to print")
+parser.add_argument("--discoal",action="store_true")
 args = parser.parse_args()
 
 
-def stairs2ms(inFile, locLen, rhorat, epochs):
+def stairs2ms(inFile, locLen, rhorat, epochs, discoal):
     """
     """
     msmc2ms2 = []
     with open(inFile,  'r') as stair:
-        next(stair)
-        line = next(stair)
-        x = line.split()
-        theta0M = float(x[2])
-        theta0L = float(x[3])
-        theta0U = float(x[4])
-        Ne0 = float(x[6])
         for line in stair:
-            x = line.split()
-            theta = float(x[2])
-            thetaL = float(x[3])
-            thetaU = float(x[4])
-            gens = float(x[5])
-            Ne = float(x[6])
-            msmc2ms2.append([gens/(4*Ne0), theta/theta0M])
+            if line.startswith("mutation"):
+                line = next(stair)
+                x = line.split()
+                theta0M = float(x[2])
+                theta0L = float(x[3])
+                theta0U = float(x[4])
+                Ne0 = float(x[6])
+                for line in stair:
+                    x = line.split()
+                    theta = float(x[2])
+                    thetaL = float(x[3])
+                    thetaU = float(x[4])
+                    gens = float(x[5])
+                    Ne = float(x[6])
+                    msmc2ms2.append([gens/(4*Ne0), theta/theta0M])
     if epochs:
         reducedEpochs = int(len(msmc2ms2)/epochs)
         msmc2ms = msmc2ms2[0::reducedEpochs]
@@ -50,9 +52,12 @@ def stairs2ms(inFile, locLen, rhorat, epochs):
     size = ' -en '.join(' '.join(str(x) for x in timestep) for timestep in msmc2ms)
     print("-Pt {} {}".format(tL, tU))
     print("-Pr {} {}".format(tL*rhorat, tU*rhorat))
-    print("-t {} -r {} -en {}".format(tM, tM * rhorat, size))
+    if discoal:
+        print("-t {} -r {} -en 0 {}".format(tM, tM * rhorat, size))
+    else:
+        print("-t {} -r {} -en {}".format(tM, tM * rhorat, size))
     return(None)
 
 
 if __name__ == "__main__":
-    stairs2ms(args.inFile, args.LocusLen, args.rhorat, args.epochs)
+    stairs2ms(args.inFile, args.LocusLen, args.rhorat, args.epochs, args.discoal)
