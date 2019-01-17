b8c66009f3625088fca23950671bc69abdfdce0b
smalls <stsmall@gmail.com>
2019-01-17 17:36:10 -0500

    d

--- sfsFromFasta.py
+++ sfsFromFasta.py
@@ -18,11 +18,12 @@ parser.add_argument('-anc', "--ancestral", type=str, help="position of the ances
 args = parser.parse_args()
 
 
-def sfs(fastaFile, ancFile):
+def sfs(fastaFile, ancFile, step=1000):
     """
     """
     alignlist = []
     totalbp = 0
+    thinbp = 0
     fasta_sequences = list(SeqIO.parse(fastaFile, 'fasta'))
     # load fasta
     for fasta in fasta_sequences:
@@ -34,6 +35,8 @@ def sfs(fastaFile, ancFile):
         ancseq = str(fasta.seq)
     # calc sfs derived
     sfsarray = np.zeros(len(alignlist))
+    sfsarray_thin = np.zeros(len(alignlist))
+    thinStart = 0
     for bp in range(len(ancseq)):
         anc = ancseq[bp]
         al = [seq[bp] for seq in alignlist]
@@ -47,22 +50,32 @@ def sfs(fastaFile, ancFile):
                 continue
             elif alleles.size == 1:
                 totalbp += 1
+                if bp - thinStart > step:
+                    thinStart = bp
+                    thinbp += 1
             else:
                 try:
                     ix = list(alcnt[0]).index(anc)
                     if ix == 0:
                         sfsarray[freq[1]] += 1
+                        if bp - thinStart > step:
+                            sfsarray_thin[freq[1]] += 1
+                            thinStart = bp
+                            thinbp += 1
                     else:
                         sfsarray[freq[0]] += 1
+                        if bp - thinStart > step:
+                            sfsarray_thin[freq[0]] += 1
+                            thinStart = bp
+                            thinbp += 1
                     totalbp += 1
                 except ValueError:
                     continue
-    print(totalbp)
-    return(sfsarray)
+    print(totalbp, thinbp)
+    return(sfsarray, sfsarray_thin)
 
 
 if __name__ == "__main__":
-    sfs = sfs(args.fasta, args.ancestral)
-    print(sfs)
-
-
+    sfs, sfst = sfs(args.fasta, args.ancestral)
+    print("{}".format(" ".join(map(str, sfs))))
+    print("{}".format(" ".join(map(str, sfst))))
