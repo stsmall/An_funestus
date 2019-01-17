1d8eac49f584885065ba051102c5e0c6e1d299aa
smalls <stsmall@gmail.com>
2019-01-17 13:33:10 -0500

    dirty and slow sfs from fasta alignment

---
+++ sfsFromFasta.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 17 12:57:29 2019
+
+@author: scott
+"""
+
+from __future__ import print_function
+from __future__ import division
+import argparse
+from Bio import SeqIO
+import numpy as np
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-f', "--fasta", type=str, required=True, help="fasta file")
+parser.add_argument('-anc', "--ancestral", type=str, help="position of the ancestral")
+args = parser.parse_args()
+
+
+def sfs(fastaFile, ancFile):
+    """
+    """
+    alignlist = []
+    totalbp = 0
+    fasta_sequences = list(SeqIO.parse(fastaFile, 'fasta'))
+    # load fasta
+    for fasta in fasta_sequences:
+        sequence = str(fasta.seq)
+        alignlist.append(sequence)
+    # load ancestral seq for polarization
+    fasta_anc = list(SeqIO.parse(ancFile, 'fasta'))
+    for fasta in fasta_anc:
+        ancseq = str(fasta.seq)
+    # calc sfs derived
+    sfsarray = np.zeros(len(alignlist))
+    for bp in range(len(ancseq)):
+        anc = ancseq[bp]
+        al = [seq[bp] for seq in alignlist]
+        if "N" in anc or "N" in al:
+            continue
+        else:
+            alcnt = np.unique(al, return_counts=True)
+            alleles = alcnt[0]
+            ix = list(alcnt[0]).index(anc)
+            freq = alcnt[1]
+            if alleles.size > 2:
+                continue
+            else:
+                if ix == 0:
+                    sfsarray[freq[1]] += 1
+                else:
+                    sfsarray[freq[0]] += 1
+                totalbp += 1
+    print(totalbp)
+    return(sfsarray)
+
+
+if __name__ == "__main__":
+    sfs = sfs(args.fasta, args.ancestral)
+    print(sfs)
+
+
