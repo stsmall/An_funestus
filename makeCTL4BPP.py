dc4f5426f0a9a32c0f9f9b506d1d2f374e9572f9
smalls <stsmall@gmail.com>
2019-12-12 14:09:44 -0500

    makeCTL4BPP - add locus count

--- makeCTL4BPP.py
+++ makeCTL4BPP.py
@@ -10,17 +10,21 @@ import glob
 ctl_files = glob.glob("*.txt")
 c = 1
 for ctl in ctl_files:
-    loc = ctl.split(".")[3]
+    coord = ctl.split(".")[2]
+    chrom = ctl.split("."[1])
     cds = ctl.split(".")[0]
-    f = open("A01.bpp.{}.{}.ctl".format(cds, loc), 'w')
+    counts = ctl.split(".")[3]
+    f = open(f"A01.bpp.{cds}.{chrom}.{coord}.ctl", 'w')
     with open(sys.argv[1], 'r') as Sctl:
         for line in Sctl:
             if line.startswith("seqfile"):
                 f.write("seqfile = {}\n".format(ctl))
             elif line.startswith("outfile"):
                 f.write("outfile = {}.out\n".format(".".join(ctl.split(".")[0:-1])))
+            elif line.startswith("nloci"):
+                f.write(f"nloci = {counts}\n")
             elif line.startswith("mcmcfile"):
-                f.write("mcmcfile = {}.msmc.txt\n".format(".".join(ctl.split(".")[0:-1])))
+                f.write("mcmcfile = {}.mcmc.txt\n".format(".".join(ctl.split(".")[0:-1])))
             else:
                 f.write(line)
     c += 1
