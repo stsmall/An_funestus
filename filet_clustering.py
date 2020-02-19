fb4824a9f1b4f83c94abbe9cc83beea0693e20cd
smalls <stsmall@gmail.com>
2020-02-18 19:18:08 -0500

    d

--- filet_clustering.py
+++ filet_clustering.py
@@ -11,12 +11,14 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-f', "--InFile", required=True, help="filet output file")
-parser.add_argument('-p1', "--prob1", type=float, default=0.90,
-                    help="prob noIntrog")
+parser.add_argument('-p1', "--prob1", type=float, default=0.10,
+                    help="prob of noMig class")
+parser.add_argument('-p2', "--prob2", type=float, default=0.90,
+                    help="cut off of single class rolling mean")
 args = parser.parse_args()
 
 
-def clusterIntrogressedRegions(InFile, p1):
+def clusterIntrogressedRegions(InFile, p1, p2):
     """
 
     """
@@ -30,63 +32,48 @@ def clusterIntrogressedRegions(InFile, p1):
                 chrom = x[0]
                 start = int(x[1])
                 end = int(x[2])
+                sites = int(x[3])
                 pred = x[4]
-                noMig = float(x[5])
-                mig12 = float(x[6])
-                mig21 = float(x[7])
-                if (1-noMig) >= p1:
+                noMigp = float(x[5])
+                mig12p = float(x[6])
+                mig21p = float(x[7])
+                if noMigp <= p1:
                     if pred == '1':
-                        clustlist.append(mig12)
-                        rollmean = p1
+                        clustlist.append(mig12p)
                         line = next(filet)
                         clustlist.append(float(line.split()[6]))
                         rollmean = np.mean(clustlist)
-                        noMig = float(line.split()[5])
-                        while (1-noMig) >= p1 and rollmean >= p1:
-                            line = next(filet)
-                            pred += line.split()[4]
+                        noMigp = float(line.split()[5])
+                        while noMigp <= p1 and rollmean >= p2:
                             end = int(line.split()[2])
+                            pred += line.split()[4]
+                            sites += int(line.split()[3])
+                            line = next(filet)
                             clustlist.append(float(line.split()[6]))
                             rollmean = np.mean(clustlist)
-                            noMig = float(line.split()[5])
+                            noMigp = float(line.split()[5])
                     elif pred == '2':
-                        clustlist.append(mig21)
-                        rollmean = p1
+                        clustlist.append(mig21p)
                         line = next(filet)
                         clustlist.append(float(line.split()[7]))
                         rollmean = np.mean(clustlist)
-                        noMig = float(line.split()[5])
-                        while (1-noMig) >= p1 and rollmean >= p1:
-                            line = next(filet)
-                            pred += line.split()[4]
+                        noMigp = float(line.split()[5])
+                        while noMigp <= p1 and rollmean >= p2:
                             end = int(line.split()[2])
+                            pred += line.split()[4]
+                            sites += int(line.split()[3])
+                            line = next(filet)
                             clustlist.append(float(line.split()[7]))
                             rollmean = np.mean(clustlist)
-                            noMig = float(line.split()[5])
-                    else:
-                        pl = [mig12, mig21]
-                        ix = pl.index(max(pl)) + 6
-                        clustlist.append(max(pl))
-                        rollmean = p1
-                        line = next(filet)
-                        clustlist.append(float(line.split()[ix]))
-                        rollmean = np.mean(clustlist)
-                        noMig = float(line.split()[5])
-                        while (1-noMig) >= p1 and rollmean >= p1:
-                            line = next(filet)
-                            pred += line.split()[4]
-                            end = int(line.split()[2])
-                            clustlist.append(float(line.split()[ix]))
-                            rollmean = np.mean(clustlist)
-                            noMig = float(line.split()[5])
-                    f.write("{}\t{}\t{}\t{}\t{}\n".format(chrom, start, end, pred, max(clustlist)))
-                clustlist = []
+                            noMigp = float(line.split()[5])
+                    f.write("{}\t{}\t{}\t{}\t{}\t{}\n".format(chrom, start, end, sites, pred, rollmean))
+                    clustlist = []
             except StopIteration:
-                f.write("{}\t{}\t{}\t{}\t{}\n".format(chrom, start, end, pred, max(clustlist)))
+                f.write("{}\t{}\t{}\t{}\t{}\t{}\n".format(chrom, start, end, sites, pred, rollmean))
                 break
     f.close()
     return(None)
 
 
 if __name__ == "__main__":
-    clusterIntrogressedRegions(args.InFile, args.prob1)
+    clusterIntrogressedRegions(args.InFile, args.prob1, args.prob2)
